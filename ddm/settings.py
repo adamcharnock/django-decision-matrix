4a073f48819e326b78ec24aa840f2f9169371f6a
Adam Charnock <acharnock@twitter.com>
2016-06-25 14:06:18 +0100

    Setting ALLOWED_HOSTS

--- ddm/settings.py
+++ ddm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
